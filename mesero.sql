e149117ef0a27d1cd1965d62f9feb316ea21bca2
Yesenia Leonel <yesenialeonelv@gmail.com>
2019-10-24 02:26:34 -0500

    SQL2

--- mesero.sql
+++ mesero.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2019 at 04:34 AM
+-- Generation Time: Oct 24, 2019 at 09:25 AM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.2.14
 
@@ -41,7 +41,10 @@ CREATE TABLE `bebida` (
 
 INSERT INTO `bebida` (`ID_bebida`, `nombre_b`, `clasificacion`, `descripcion_b`) VALUES
 (1, 'Frapuccino (Clasico)', 1, 'Es un café con hielo cubierto de espuma elaborado a partir de café instantáneo.'),
-(2, 'Mocha (Chocolate)', 1, 'Es una variante del café con leche. \r\nPreparado con un tercio de expreso y dos tercios de leche vaporizada,añadiendo una parte de jarabe de chocolate, agregando la famosa espuma de leche encima y canela.');
+(2, 'Mocha (Chocolate)', 1, 'Es una variante del café con leche. \r\nPreparado con un tercio de expreso y dos tercios de leche vaporizada,añadiendo una parte de jarabe de chocolate, agregando la famosa espuma de leche encima y canela.'),
+(3, 'Chai (Especies)', 1, 'Consiste en una mezcla de té con especias e hierbas aromáticas'),
+(4, 'Espresso', 2, 'Se caracteriza por su rápida preparación y por un sabor y textura más concentrados'),
+(5, 'Americano', 2, 'El café americano es la mezcla de agua caliente y café procesado en un máquina de Expreso');
 
 -- --------------------------------------------------------
 
@@ -59,7 +62,12 @@ CREATE TABLE `clasificacion` (
 --
 
 INSERT INTO `clasificacion` (`ID_clasificacion`, `nombre_clasi`) VALUES
-(1, 'Frio');
+(1, 'Frio'),
+(2, 'Caliente'),
+(3, 'Desayuno'),
+(4, 'ComidaSaludable'),
+(5, 'ComidaRapida'),
+(6, 'Postre');
 
 -- --------------------------------------------------------
 
@@ -84,6 +92,32 @@ CREATE TABLE `persona` (
 
 INSERT INTO `persona` (`ID_persona`, `nombre_p`, `apellido1_p`, `apellido2_p`, `fnacimiento`, `rolF`, `email`, `password`) VALUES
 (1, 'Yesenia', 'Leonel', 'Vera', '1996-08-29', 2, 'yesenia@gmial.com', '12345678');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `platillo`
+--
+
+CREATE TABLE `platillo` (
+  `ID_platillo` int(11) NOT NULL,
+  `nombre_platillo` varchar(50) NOT NULL,
+  `clasificacion_p` int(11) NOT NULL,
+  `descripcion_p` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `platillo`
+--
+
+INSERT INTO `platillo` (`ID_platillo`, `nombre_platillo`, `clasificacion_p`, `descripcion_p`) VALUES
+(2, 'Sandwich tradicional', 3, 'Jamon, Queso mozarella, Lechuga y Tomate con mayonesa'),
+(3, 'Enfrijoladas', 3, 'Platillo elaborado con tortillas de maíz que se sumergen en un caldillo espeso de frijoles molidos, enrolladas o dobladas, se sirven en un plato, con queso, cebolla y crema.'),
+(4, 'Madre Selva', 4, 'Ensalada con delicada mezcla de lechuga, durazno, queso mosarella, nuez de brasil triturada y trozos de filete de pollo asado con suave aderezo de aceite de oliva.'),
+(5, 'César', 4, 'Es una ensalada de lechuga romana y croûtons con jugo de limón, aceite de oliva, huevo, salsa Worcestershire, anchoas, ajo, mostaza de Dijon, queso parmesano y pimienta negra'),
+(6, 'Hamburguesa de filete de pollo', 5, '150gr de pollo marinado en mostaza, en pan Artillano con mayonesa de menta al limón, queso Mozarella asado, lechuga, tomate, pepinillos agridulces, licor de naranja.'),
+(7, 'Babel', 6, 'Brownie sobre reduccion de balsamico y vino tinto mas helado citron coronado con helado de frutos rojos y galleta de queso parmesano.'),
+(8, 'Ursula', 6, 'Torre de suave tortas de cafe marinadas en amaretto, licor de café y ron, entre capas de chocolate, terminada con crema chantilly y frutas de temporada.');
 
 -- --------------------------------------------------------
 
@@ -132,6 +166,13 @@ ALTER TABLE `persona`
   ADD KEY `rolF` (`rolF`);
 
 --
+-- Indexes for table `platillo`
+--
+ALTER TABLE `platillo`
+  ADD PRIMARY KEY (`ID_platillo`),
+  ADD KEY `clasificacion_p` (`clasificacion_p`);
+
+--
 -- Indexes for table `rol`
 --
 ALTER TABLE `rol`
@@ -145,19 +186,25 @@ ALTER TABLE `rol`
 -- AUTO_INCREMENT for table `bebida`
 --
 ALTER TABLE `bebida`
-  MODIFY `ID_bebida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `ID_bebida` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `clasificacion`
 --
 ALTER TABLE `clasificacion`
-  MODIFY `ID_clasificacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `ID_clasificacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `persona`
 --
 ALTER TABLE `persona`
   MODIFY `ID_persona` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `platillo`
+--
+ALTER TABLE `platillo`
+  MODIFY `ID_platillo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `rol`
@@ -180,6 +227,12 @@ ALTER TABLE `bebida`
 --
 ALTER TABLE `persona`
   ADD CONSTRAINT `persona_ibfk_1` FOREIGN KEY (`rolF`) REFERENCES `rol` (`ID_rol`);
+
+--
+-- Constraints for table `platillo`
+--
+ALTER TABLE `platillo`
+  ADD CONSTRAINT `platillo_ibfk_1` FOREIGN KEY (`clasificacion_p`) REFERENCES `clasificacion` (`ID_clasificacion`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
