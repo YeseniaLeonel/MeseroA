a230e2289400b950dc4bc43a2bf1f395c1df0c84
Luis García <luis_grdz@hotmail.com>
2019-10-26 21:11:18 -0500

    Modification: settings to work in Codeigniter

--- application/views/layouts/body.php
+++ application/views/layouts/body.php
@@ -7,16 +7,21 @@
                   <div class="col-md-8 col-md-offset-2">
                     <div class="home-content">
                       <div class="home-heading">
-                        <h1><em>Stak</em> HTML CSS</h1>
+                        <h1><em>Comanda</em> Automática</h1>
                       </div>
                       <div class="row">
                         <div class="col-md-12">
                           <div class="home-box-content">
                             <div class="left-text">
-                              <h4>New <em>Stacked</em> Template</h4>
-                              <p>Stacked is brought to you by TemplateMo. Lorem ipsum dolor sit amet, consectetur adipiscing elit etiam sit amet ante vehicula, aliquam mauris in, luctus neque.</p>
+                              <h4>Una <em>Nueva</em> Forma de ordenar</h4>
+                              <p>Para comenzar a ordenar de manera automática, es necesario que te registres en el sistema</p>
                               <div class="primary-button">
-                                <a href="#2">Discover More</a>
+                                <a href="#2">Descubre Cómo</a>
+                              </div>
+                              <br>
+                              <br>
+                              <div class="primary-button">
+                                <a href="#5">Ya tengo una cuenta</a>
                               </div>
                             </div>
                             <div class="right-image">
@@ -40,7 +45,7 @@
                   <div class="col-md-8 col-md-offset-2">
                     <div class="about-content">
                       <div class="heading">
-                        <h4>About us</h4>
+                        <h4>Para Empezar</h4>
                       </div>
                       <div class="row">
                         <div class="col-md-12">
@@ -50,10 +55,10 @@
                         </div>
                         <div class="col-md-7 col-md-offset-5">
                           <div class="about-box-text">
-                            <h4>Curabitur varius sapien</h4>
-                            <p>Nulla vel accumsan dui. Aliquam tincidunt elit elit, nec iaculis neque interdum dictum. Donec venenatis quam enim, eu mattis arcu rhoncus id. Ut ac urna tellus.</p>
+                            <h4>Qué Datos Necesitamos?</h4>
+                            <p>No será demasiado, solo requerimos que nos proporciones tu nombre y número telefónico para registrarte y comenzar a ordenar</p>
                             <div class="primary-button">
-                              <a href="#3">Discover More</a>
+                              <a href="#3">Continuar la Guía</a>
                             </div>
                           </div>
                         </div>
@@ -73,7 +78,7 @@
                   <div class="col-md-8 col-md-offset-2">
                     <div class="projects-content">
                       <div class="heading">
-                        <h4>Recent Projects</h4>
+                        <h4>Qué nos hace diferentes</h4>
                       </div>
                       <div class="row">
                         <div class="col-md-12">
@@ -82,10 +87,10 @@
                               <div class="project-item">
                                 <a href="<?php echo base_url('assets/template/img/project-item-01.jpg'); ?>" data-lightbox="image-1"><img src="<?php echo base_url('assets/template/img/project-item-01.jpg'); ?>" alt=""></a>
                                 <div class="text-content">
-                                  <h4>Best Design</h4>
-                                  <p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
+                                  <h4>Ordena Facilmente</h4>
+                                  <p>Registrate con unos cuantos pasos y comienza a armar tu órden.</p>
                                   <div class="primary-button">
-                                    <a href="#">Discover More</a>
+                                    <a href="#4">Discover More</a>
                                   </div>
                                 </div>
                               </div>
@@ -94,10 +99,10 @@
                               <div class="project-item">
                                 <a href="<?php echo base_url('assets/template/img/project-item-02.jpg'); ?>" data-lightbox="image-1"><img src="<?php echo base_url('assets/template/img/project-item-02.jpg'); ?>" alt=""></a>
                                 <div class="text-content">
-                                  <h4>Creative Pen</h4>
-                                  <p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
+                                  <h4>No Hagas Filas</h4>
+                                  <p>Nuestro sistema está diseñado para facilitarte el servicio sin que hagas filas, ordenando, desde la comodidad de tu mesa.</p>
                                   <div class="primary-button">
-                                    <a href="#">Discover More</a>
+                                    <a href="#4">Discover More</a>
                                   </div>
                                 </div>
                               </div>
@@ -106,10 +111,10 @@
                               <div class="project-item">
                                 <a href="<?php echo base_url('assets/template/img/project-item-03.jpg'); ?>" data-lightbox="image-1"><img src="<?php echo base_url('assets/template/img/project-item-03.jpg'); ?>" alt=""></a>
                                 <div class="text-content">
-                                  <h4>Nice Capture</h4>
-                                  <p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
+                                  <h4>Solicita tu Órden</h4>
+                                  <p>Una vez concluida tu orden, uno de nuestros meseros te atenderá para confirmarla.</p>
                                   <div class="primary-button">
-                                    <a href="#">Discover More</a>
+                                    <a href="#4">Discover More</a>
                                   </div>
                                 </div>
                               </div>
@@ -118,10 +123,10 @@
                               <div class="project-item">
                                 <a href="<?php echo base_url('assets/template/img/project-item-04.jpg'); ?>" data-lightbox="image-1"><img src="<?php echo base_url('assets/template/img/project-item-04.jpg'); ?>" alt=""></a>
                                 <div class="text-content">
-                                  <h4>This Moment</h4>
-                                  <p>Lorem ipsum dolor sit amet, adipis scing elit etiam sit amet ante vehicula, aliquam mauris in, luctus neque.</p>
+                                  <h4>Consulta los Tiempos</h4>
+                                  <p>Una vez confirmada por uno de nuestros meseros, podrás consultar el tiempo aproximado de espera para recibir tu orden.</p>
                                   <div class="primary-button">
-                                    <a href="#">Discover More</a>
+                                    <a href="#4">Discover More</a>
                                   </div>
                                 </div>
                               </div>
@@ -130,10 +135,10 @@
                               <div class="project-item">
                                 <a href="<?php echo base_url('assets/template/img/project-item-05.jpg'); ?>" data-lightbox="image-1"><img src="<?php echo base_url('assets/template/img/project-item-05.jpg'); ?>" alt=""></a>
                                 <div class="text-content">
-                                  <h4>Artwork</h4>
-                                  <p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
+                                  <h4>Ordena Más</h4>
+                                  <p>En caso de que requieras ordenar algo más de nuestro menú, lo puedes hacer, y uno de nuestros meseros te atenderá para confirmar tu órden</p>
                                   <div class="primary-button">
-                                    <a href="#">Discover More</a>
+                                    <a href="#4">Discover More</a>
                                   </div>
                                 </div>
                               </div>
@@ -142,10 +147,10 @@
                               <div class="project-item">
                                 <a href="<?php echo base_url('assets/template/img/project-item-06.jpg'); ?>" data-lightbox="image-1"><img src="<?php echo base_url('assets/template/img/project-item-06.jpg'); ?>" alt=""></a>
                                 <div class="text-content">
-                                  <h4>Item Sixth</h4>
-                                  <p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
+                                  <h4>Cuánto Pagarás</h4>
+                                  <p>Puedes consultar tu cuenta cuando lo necesites, y solicitar al mesero realice el cobro de la misma si así lo requieres.</p>
                                   <div class="primary-button">
-                                    <a href="#">Discover More</a>
+                                    <a href="#4">Discover More</a>
                                   </div>
                                 </div>
                               </div>
@@ -154,10 +159,10 @@
                               <div class="project-item">
                                 <a href="<?php echo base_url('assets/template/img/project-item-07.jpg'); ?>" data-lightbox="image-1"><img src="<?php echo base_url('assets/template/img/project-item-07.jpg'); ?>" alt=""></a>
                                 <div class="text-content">
-                                  <h4>Last Item</h4>
-                                  <p>Lorem ipsum dolor, adipis scing elit etiam ante vehicula, aliquam mauris in, luctus neque.</p>
+                                  <h4>Te estaremos esperando</h4>
+                                  <p>Después de visitarnos, estaremos listos para ofrecerte lo que más te gusta; un rápido y excelente servicio.</p>
                                   <div class="primary-button">
-                                    <a href="#">Discover More</a>
+                                    <a href="#4">Discover More</a>
                                   </div>
                                 </div>
                               </div>
@@ -180,41 +185,37 @@
                   <div class="col-md-8 col-md-offset-2">
                     <div class="contact-content">
                       <div class="heading">
-                        <h4>Contact us</h4>
+                        <h4>Regístrate</h4>
                       </div>
                       <div class="row">
                         <div class="col-md-8">
                           <div class="contat-form">
                             <form id="contact" action="" method="post">
                               <fieldset>
-                                <input name="name" type="text" class="form-control" id="name" placeholder="Your Name" required>
+                                <input name="name" type="text" class="form-control" id="name" placeholder="Tu Nombre" required>
                               </fieldset>
                               <fieldset>
-                                <input name="email" type="email" class="form-control" id="email" placeholder="Email" required>
+                                <input name="telefono" type="text" class="form-control" id="email" placeholder="Tu número telefónico" required>
                               </fieldset>
+                              <!-- <fieldset>
+                                <textarea name="message" rows="6" class="form-control" id="message" placeholder="Déjanos algún comentario..." required></textarea>
+                              </fieldset> -->
                               <fieldset>
-                                <textarea name="message" rows="6" class="form-control" id="message" placeholder="Message" required></textarea>
-                              </fieldset>
-                              <fieldset>
-                                <button type="submit" id="form-submit" class="btn">Send Message</button>
+                                <button type="submit" id="form-submit" class="btn">Enviar mi Registro</button>
                               </fieldset>
                             </form>
                           </div>
                         </div>
                         <div class="col-md-4">
                           <div class="more-info">
-                            <p>Lorem ipsum dolor , consectetur adipiscing elit. Nunc purus ligula, ullamcorper id velit id, vestibulum auc sapien. Sed quis mauris eget sem imperdiet rhoncus.<br><br>
-                            <em>3344 Etiam mauris erat,
-								<br>Vestibulum eu augue nec, 10550</em></p>
+                            <p>Recuerda, los datos que nos proporciones solo serán utilizados para darte una mejor atención en nuestro restaurante.<br><br>
+                              <em>Gracias por tu preferencia
+                                <br>
+                              </em>
+                            </p>
                           </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                  <div class="col-md-12">
-                    <div class="footer">
-                      <p>Copyright &copy; 2017 Your Company 
-                      | Designed by TemplateMo</p>
                     </div>
                   </div>
                 </div>
@@ -222,5 +223,49 @@
             </div>
           </article>
         </section>
-      
+
+        <section class="panel b-green" id="5">
+          <article class="panel__wrapper">
+            <div class="panel__content">
+              <div class="container">
+                <div class="row">
+                  <div class="col-md-8 col-md-offset-2">
+                    <div class="contact-content">
+                      <div class="heading">
+                        <h4>Si ya tienes cuenta, a continuación ingresa tus datos</h4>
+                      </div>
+                      <div class="row">
+                        <div class="col-md-8">
+                          <div class="contat-form">
+                            <form id="contact" action="" method="post">
+                              <fieldset>
+                                <input name="name" type="text" class="form-control" id="name" placeholder="Tu Nombre" required>
+                              </fieldset>
+                              <fieldset>
+                                <input name="telefono" type="text" class="form-control" id="email" placeholder="Tu número telefónico" required>
+                              </fieldset>
+                              <!-- <fieldset>
+                                <textarea name="message" rows="6" class="form-control" id="message" placeholder="Déjanos algún comentario..." required></textarea>
+                              </fieldset> -->
+                              <fieldset>
+                                <button type="submit" id="form-submit" class="btn">Ingresar</button>
+                              </fieldset>
+                            </form>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col-md-12">
+                    <div class="footer">
+                      <p>Copyright &copy; 2019 Comanda Automática 
+                      | Designed by LuigisDev</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+        </section>
+
 
