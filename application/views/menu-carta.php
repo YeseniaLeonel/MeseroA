e20b600ee286051f1ea5863053299dedd80575a3
Luis García <luis_grdz@hotmail.com>
2019-11-15 23:27:43 -0600

    Prototype for tables selection

--- application/views/menu-carta.php
+++ application/views/menu-carta.php
@@ -7,81 +7,135 @@
 	</div>
 	<br>
 	<br>
-	<div class="row">
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
+	<form>
+		<div class="row">
+			<div class="btn-group btn-group-toggle mx-auto" data-toggle="buttons">
+				<table class="table table-centered table-bordered table-hover table-striped text-center">
+					<thead>
+						<tr>
+							<th>
+								Mesa 1
+							</th>
+							<td>
+								Mesa 2
+							</td>
+							<td>
+								Mesa 3
+							</td>
+							<td>
+								Mesa 3
+							</td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<th>
+								<label class="btn btn-outline-warning active">
+									<input type="radio" name="options" id="option1" autocomplete="off" checked>
+									<div class="card text-white bg-info mb-3" >
+										<div class="card-header">Mesa 1</div>
+										<div class="card-body">
+											<h5 class="card-title">Mesero 2</h5>
+											<p class="card-text">Buen provecho.</p>
+										</div>
+									</div>
+								</label>
+							</th>
+							<td>
+								<label class="btn btn-outline-warning">
+									<input type="radio" name="options" id="option1" autocomplete="off" checked>
+									<div class="card text-white bg-info mb-3" >
+										<div class="card-header">Mesa 1</div>
+										<div class="card-body">
+											<h5 class="card-title">Mesero 2</h5>
+											<p class="card-text">Buen provecho.</p>
+										</div>
+									</div>
+								</label>					
+							</td>
+							<td>
+								<label class="btn btn-outline-warning">
+									<input type="radio" name="options" id="option1" autocomplete="off" checked>
+									<div class="card text-white bg-info mb-3" >
+										<div class="card-header">Mesa 1</div>
+										<div class="card-body">
+											<h5 class="card-title">Mesero 2</h5>
+											<p class="card-text">Buen provecho.</p>
+										</div>
+									</div>
+								</label>										
+							</td>
+							<td>
+								<label class="btn btn-outline-warning">
+									<input type="radio" name="options" id="option1" autocomplete="off" checked>
+									<div class="card text-white bg-info mb-3" >
+										<div class="card-header">Mesa 1</div>
+										<div class="card-body">
+											<h5 class="card-title">Mesero 2</h5>
+											<p class="card-text">Buen provecho.</p>
+										</div>
+									</div>
+								</label>										
+							</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>			
 		</div>
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
-		</div>
-	</div>
-	<div class="row">
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
-		</div>
-		<div class="col-md-3">
-			<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-				<div class="card-header">Header</div>
-				<div class="card-body">
-					<h5 class="card-title">Info card title</h5>
-					<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-				</div>
-			</div>
-		</div>
-	</div>
+	</form>
 
+	<div class="row">
+		<div class="btn-group btn-group-toggle mx-auto" data-toggle="buttons">
+			<label class="btn btn-outline-warning active">
+				<input type="radio" name="options" id="option1" autocomplete="off" checked>
+				<div class="card text-white bg-info mb-3" >
+					<div class="card-header">Mesa 1</div>
+					<div class="card-body">
+						<h5 class="card-title">Mesero 2</h5>
+						<p class="card-text">Buen provecho.</p>
+					</div>
+				</div>
+			</label>
+			<label class="btn btn-outline-warning">
+				<input type="radio" name="options" id="option2" autocomplete="off">
+				<div class="card text-white bg-info mb-3" >
+					<div class="card-header">Mesa 2</div>
+					<div class="card-body">
+						<h5 class="card-title">Mesero 2</h5>
+						<p class="card-text">Buen provecho.</p>
+					</div>
+				</div>
+			</label>
+			<label class="btn btn-outline-warning">
+				<input type="radio" name="options" id="option3" autocomplete="off">
+				<div class="card text-white bg-info mb-3" >
+					<div class="card-header">Mesa 3</div>
+					<div class="card-body">
+						<h5 class="card-title">Mesero 2</h5>
+						<p class="card-text">Buen provecho.</p>
+					</div>
+				</div>
+			</label>
+			<label class="btn btn-outline-warning">
+				<input type="radio" name="options" id="option3" autocomplete="off">
+				<div class="card text-white bg-info mb-3" >
+					<div class="card-header">Mesa 3</div>
+					<div class="card-body">
+						<h5 class="card-title">Mesero 2</h5>
+						<p class="card-text">Buen provecho.</p>
+					</div>
+				</div>
+			</label>
+			<label class="btn btn-outline-warning">
+				<input type="radio" name="options" id="option3" autocomplete="off">
+				<div class="card text-white bg-info mb-3" >
+					<div class="card-header">Mesa 3</div>
+					<div class="card-body">
+						<h5 class="card-title">Mesero 2</h5>
+						<p class="card-text">Buen provecho.</p>
+					</div>
+				</div>
+			</label>
+		</div>	<!-- FIN <div  class="btn-group btn-group-toggle mx-auto" data-toggle="buttons"> -->	
+	</div>
 </div>
